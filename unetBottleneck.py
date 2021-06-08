267c66979602b7ed0f632c9c8846035cf5762c03
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2021-06-08 16:17:25 +0200

    unetBottleneck

--- unetBottleneck.py
+++ unetBottleneck.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import os
 import torch
-# 170 vs 250 sec vs 120s
+
 class CnnBlock(nn.Module):
   def __init__(self, in_channels, out_channels, skip_final_activation=False):
     super().__init__()
@@ -24,12 +24,32 @@ class CnnBlock(nn.Module):
       x = self.activation(x)
     return x
 
+class StackedBlocks(nn.Module):
+  def __init__(self, in_channels, out_channels, n_blocks):
+    super().__init__()
+    self.n_blocks = n_blocks
+    if n_blocks == 0: # one Conv2d
+      self.blocks = nn.Sequential(nn.Conv2d(in_channels, out_channels, 3, stride=1, padding=1), nn.ReLU())
+    elif n_blocks == 1: # two Conv2d with a skip connection
+      self.blocks = CnnBlock(in_channels, out_channels)
+    elif n_blocks > 1:
+      self.blocks = nn.Sequential(CnnBlock(in_channels, out_channels), *[CnnBlock(out_channels, out_channels) for i in range(n_blocks-1)])
+      self.skip = nn.Conv2d(in_channels, out_channels, 1)
+    else:
+      raise ValueError("n_blocks must be larger than 0, it was:", n_blocks)
+
+  def forward(self, x):
+    input_x = x
+    x = self.blocks(x)
+    return self.skip(input_x) + x if self.n_blocks > 1 else x
+
 class Encoder(nn.Module):
-  def __init__(self, nchannels):
+  def __init__(self, nchannels, nblocks):
     super().__init__()
     self.nchannels = nchannels
     self.pool = nn.MaxPool2d(2)
-    self.blocks = nn.ModuleList([CnnBlock(nchannels[i], nchannels[i+1]) for i in range(len(nchannels)-1)])
+    self.blocks = nn.ModuleList([StackedBlocks(nchannels[i], nchannels[i+1], nblocks) for i in range(len(nchannels)-2)])
+    self.blocks.append(CnnBlock(nchannels[-2], nchannels[-1]))
 
   def forward(self, x):
     features = []
@@ -46,13 +66,12 @@ class UpscaleBlock(nn.Module): # A*A*C -> 2A*2A*C/2
     self.upscaleLayer = nn.Upsample(scale_factor=2, mode='nearest')
     self.pad = nn.ReflectionPad2d(1)
     self.conv1 = nn.Conv2d(in_channels, out_channels, 3, padding=0, stride=1)
-    self.activation = F.relu
+    self.activation = nn.LeakyReLU(negative_slope=0.2, inplace=True)
 
   def forward(self, x):
     x = self.pad(self.upscaleLayer(x))
     x = self.conv1(x)
-    # activation function here?
-    #x = self.activation(x)
+    x = self.activation(x)
     return x
 
 class Decoder(nn.Module):
@@ -61,8 +80,7 @@ class Decoder(nn.Module):
     self.nchannels = nchannels
     self.upconvs = nn.ModuleList([UpscaleBlock(nchannels[i], nchannels[i]//2) for i in range(len(nchannels)-1)])
     self.blocks = nn.ModuleList([CnnBlock(nchannels[i], nchannels[i+1]) for i in range(len(nchannels)-1)])
-    self.finalUpscale = UpscaleBlock(nchannels[-1], nchannels[-1])
-    self.finalBlock = CnnBlock(nchannels[-1], 3, skip_final_activation=True)
+    self.finalBlocks = nn.Sequential(nn.Conv2d(nchannels[-1], nchannels[-1], 3, padding=1), nn.LeakyReLU(negative_slope=0.2, inplace=True), nn.Conv2d(nchannels[-1], 2, 3, padding=1))
 
   def forward(self, x, encoder_features):
     for i in range(len(self.nchannels)-1):
@@ -72,19 +90,18 @@ class Decoder(nn.Module):
       temp = encoder_features[i]
       del temp
       encoder_features[i] = None
-    x = self.finalUpscale(x)
-    x = torch.sigmoid(self.finalBlock(x))
+    x = self.finalBlocks(x)
     return x
 
 class UNet(nn.Module):
   # Important! The side lengths of the input image must be divisible depth times by 2. Add padding to nearest multiple when evaluating
   # Safe size: current_size + current_size % 2**(len(nchannels)-1) 
   # Pad to safe size, then crop to correct upscaled size afterwards
-  def __init__(self, depth=4, init_channels=64):
+  def __init__(self, depth=5, init_channels=64, nblocks=1):
     super().__init__()
     #nchannels=[64,128,256,512]
     self.nchannels = [init_channels * 2**i for i in range(depth)]
-    self.encoder = Encoder([3] + self.nchannels)
+    self.encoder = Encoder([1] + self.nchannels, nblocks)
     self.decoder = Decoder(self.nchannels[::-1]) # reverse
 
   def forward(self, x):
